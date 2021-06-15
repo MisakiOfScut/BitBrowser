4eb95b7d92206b1929946133b9ab192b80b4f9e5
Vivianlhm <2439590694@qq.com>
2021-06-15 14:30:57 +0800

    InfoModalView我的弹出框

--- BitBrowser/InfoModal/InfoModalView.swift
+++ BitBrowser/InfoModal/InfoModalView.swift
@@ -8,31 +8,110 @@
 import SwiftUI
 
 struct InfoModalView: View {
-    @Binding var showModal: Bool
+//    @Binding var showModal: Bool
+    @State private var isLogin = true
+    @State private var userName = "vivian"
+    @State private var showAlert = false
+    @State private var showQuit = false
+    @State private var isBookmark = false
+    @State private var isHistory = false
+    @State private var isLoginPage = false
+    
+//    func changeLoginState(){
+//        isLogin = !isLogin
+//    }
+    
+    @Environment(\.presentationMode) private var presentationMode
+    
     var body: some View {
         NavigationView {
             VStack {
-                Button("关闭") {
-                    self.showModal = false
-                }
-                HStack {
-                    Button("登录") {
+                VStack(spacing: 0) {
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 50.0, height: 50)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            .shadow(radius: 3)
+                        Text(isLogin ? userName : "您尚未登录")
+                            .padding(.leading, 6.0)
+                        Spacer()
+//                        NavigationLink(
+//                            destination: LoginView()) {
+                            Text(isLogin ? "退出登录" : "登录/注册")
+                                .foregroundColor(Color("System_Blue"))
+                                .onTapGesture {
+                                    isLoginPage = true
+                                }
+                                .fullScreenCover(isPresented: $isLoginPage, content: {
+                                    LoginView()
+                                })
+//                        }
+                        .ignoresSafeArea(edges: .top)
+                        .navigationBarHidden(true)
+                        .disabled(isLogin)
+                        .onTapGesture {
+                            showQuit = true
+                        }
+                        .alert(isPresented: $showQuit){
+                            Alert(title: Text("确定退出吗"),
+                                  primaryButton: .default(Text("确定")){
+                                    isLogin = false
+                                  },
+                                  secondaryButton: .destructive(Text("取消")))
+                        }
+                    }
+                    .padding([.top, .leading, .trailing], 14.0)
+                    Divider().padding(.vertical, 10)
+                    HStack {
+//                        NavigationLink(destination: BookmarkView()){
+                            VStack {
+                                Image("favorite")
+                                    .scaleEffect(CGSize(width: 1, height: 1))
+                                Text("书签")
+                                    .foregroundColor(Color("System_Blue"))
+                            }
+                            .onTapGesture {
+                                isBookmark = true
+                            }
+                            .fullScreenCover(isPresented: $isBookmark, content: {
+                                BookmarkView()
+                            })
+//                        }
                         
+//                        NavigationLink(destination: HistoryView()){
+                            VStack {
+                                Image("history")
+                                    .scaleEffect(CGSize(width: 1, height: 1))
+                                Text("历史记录")
+                                    .foregroundColor(Color("System_Blue"))
+                            }
+                            .onTapGesture {
+                                isHistory = true
+                            }
+                            .fullScreenCover(isPresented: $isHistory, content: {
+                                HistoryView()
+                            })
+//                        }
+                        Spacer()
                     }
-                    Button("注册") {
-                        
+                    .disabled(!isLogin)
+                    .onTapGesture {
+                        showAlert = isLogin ? false : true
                     }
+                    .alert(isPresented: $showAlert){
+                        Alert(title: Text("请先登录"),
+                              dismissButton: .default(Text("OK")))
+                    }
+    //                .disabled(!isLogin)
+                    .padding([.top, .leading, .bottom], 14.0)
                 }
-                HStack {
-                    NavigationLink(
-                        destination: HistoryView()) {
-                        Text("历史记录")
-                    }
-                    NavigationLink(
-                        destination: BookmarkView()) {
-                        Text("书签")
-                    }
-                }.navigationBarHidden(true)
+//                .frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .frame(height: 180)
+                .background(Color("background1"))
+                .ignoresSafeArea()
+                Spacer()
             }
         }
     }
@@ -41,7 +120,8 @@ struct InfoModalView: View {
 struct InfoModalView_Previews: PreviewProvider {
 //    @State var show = true
     static var previews: some View {
-        Text("hello")
-//        InfoModalView()
+//        Text("hello")
+//        InfoModalView(showModal: .constant(true))
+        InfoModalView()
     }
 }
--- BitBrowser/common/BottomTab/BottomTabView.swift
+++ BitBrowser/common/BottomTab/BottomTabView.swift
@@ -9,48 +9,50 @@ import SwiftUI
 
 struct BottomTabView: View {
     @State var showInfoModal = false
+    @Binding var showModal: Bool
     @EnvironmentObject var web: Web
     var body: some View {
-        VStack {
-            HStack(){
-                Image("arrowLeft")
-                    .onTapGesture {
-                        web.webview.goBack()
-                    }
-                Spacer()
-                Image("arrowRight")
-                    .onTapGesture {
-                        web.webview.goForward()
-                    }
-                Spacer()
-                Image("home")
-                    .scaleEffect(CGSize(width: 0.8, height: 0.8))
-                Spacer()
-                NavigationLink(
-                    destination: BookmarkView()) {
-                    Image("favorite")
-                        .scaleEffect(CGSize(width: 0.8, height: 0.8))
+        HStack(){
+            Image("arrowLeft")
+                .onTapGesture {
+                    web.webview.goBack()
                 }
-                Spacer()
-                Image("me")
-                    .scaleEffect(0.8)
-                    .onTapGesture {
-                        self.showInfoModal = true
-                    }
-                    .sheet(isPresented: $showInfoModal) {
-                        InfoModalView(showModal: self.$showInfoModal)
-                    }
+            Spacer()
+            Image("arrowRight")
+                .onTapGesture {
+                    web.webview.goForward()
+                }
+            Spacer()
+            Image("home")
+                .scaleEffect(CGSize(width: 0.8, height: 0.8))
+            Spacer()
+            NavigationLink(
+                destination: BookmarkView()) {
+                Image("favorite")
+                    .scaleEffect(CGSize(width: 0.8, height: 0.8))
             }
-            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
-            .padding()
-            .background(Color("background1"))
+            Spacer()
+            Image("me")
+                .scaleEffect(0.8)
+                .onTapGesture {
+//                    self.showInfoModal = true
+                    self.showModal = !self.showModal
+                }
+            //                    .sheet(isPresented: $showInfoModal) {
+            //                        InfoModalView(showModal: self.$showInfoModal)
+            //                            .offset(y: 125)
+            //                        InfoModalView()
+            //                    }
             
         }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+        .padding()
+        .background(Color("background1"))
     }
 }
 
 struct BottomTabView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomTabView()
+        BottomTabView(showModal: .constant(true))
     }
 }
